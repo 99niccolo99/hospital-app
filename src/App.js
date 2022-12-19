8f299ae157f080184192e88bc4737428cc00c4d0
99niccolo99 <71765784+99niccolo99@users.noreply.github.com>
2022-12-19 18:30:37 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
-import UserData from "./components/UserData";
-import LateralBar from "./components/LateralBar";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import React, { useState } from "react";
-import Avatar from "@mui/material/Avatar";
-import CardMedia from "@mui/material/CardMedia";
-import Card from "@mui/material/Card";
-import { purple } from "@mui/material/colors";
-import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import XcAndFmLevel from "./components/Charts/XcAndFmLevel";
-import TbwFfm from "./components/Charts/Tbw";
-import BunPotCal from "./components/Charts/BunPotCal";
-import DataTable from "./components/DataTable";
-import HeartRespRate from "./components/Charts/HeartRespRate";
-import ReactDOM from "react-dom";
-import Checkbox from "@mui/material/Checkbox";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormLabel from "@mui/material/FormLabel";
-import FormControl from "@mui/material/FormControl";
-import FormGroup from "@mui/material/FormGroup";
-import FormHelperText from "@mui/material/FormHelperText";
-import FetchBox from "./components/FetchBox";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
+import React from "react";
+import FetchBox from "./components/FetchBox"
 import "./styles.css";
-import GridLayout from "./GridLayout";
-import Ccc from "./Ccc"
-import Bbb from "./Bbb"
-
 
 
 
 function App() {
-  const a = true;
 
-  const [patientExams, setPatientExams] = useState([{}]);
+  
 
-  const [heartRespRate, setHeartRespRate] = useState(false);
-  const [tbwFfm, setTbwFfm] = useState(false);
-  const [xcAndFmLevel, setXcAndFmLevel] = useState(false);
-  const [dataTable, setDataTable] = useState(false);
+  /*
 
-  const [date, setDate] = useState("9999");
-  const [gender, setGender] = useState("gener");
-  const [familyName, setFamilyName] = useState("famili");
-  const [patients, setPatients] = useState("aa");
-  const [img, setImg] = useState();
   const urlPersonalData =
     "http://localhost:8080/hospital/api/patients/TRRVLA91M13Z404A";
   const urlBalanceData =
@@ -90,20 +48,6 @@ function App() {
       });
   };
 
-
-  /*
-  <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          height: "100vh",
-          mt: 9,
-        }}
-      >
-        <LateralBar></LateralBar>
-
-        <FetchBox></FetchBox>
-      </Box>
       */
 
   return (
